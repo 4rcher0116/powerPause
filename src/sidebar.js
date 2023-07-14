3482912f3e22351b7000d73840fbf3048f23c90e
4rcher0116 <4rcher0116@gmail.com>
2023-07-14 13:49:08 -0500

    real files

---
+++ src/sidebar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BsFillCloudMoonFill } from "react-icons/bs";
+import { FaRunning } from "react-icons/fa";
+import { MdOutlineQuiz } from "react-icons/md";
+
+const Sidebar = () => {
+    return (
+        <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
+            <SideBarIcon icon ={<MdOutlineQuiz size="28"/>} />
+            <SideBarIcon icon = {<BsFillCloudMoonFill size="28"/>} />
+            <SideBarIcon icon = {<FaRunning size="28"/>} />
+        </div>
+    );
+};
+
+const SideBarIcon = ({  icon  }) =>(
+    <div className="sidebar-icon">
+        {icon}
+    </div>
+);
+
+export default Sidebar;
